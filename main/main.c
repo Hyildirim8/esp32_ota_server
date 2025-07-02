1c317299b9e53470b42e996dc9062996175e25a5
Hasan Yildirim <hystudio20@gmail.com>
2025-07-02 09:17:35 +0300

    wifi set application

--- main/main.c
+++ main/main.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "driver/gpio.h"
+/**
+ * Application entry point.
+ */
 
-#include "rgb_led.h"
-void app_main(void)
-{
-   while (true)
-   {
-    rgb_led_wifi_app_started();
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    rgb_led_http_server_started();
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    rgb_led_wifi_connected();
-    vTaskDelay(pdMS_TO_TICKS(1000));
+ #include "nvs_flash.h"
 
-   }
-   
-    
-}
+ #include "wifi_app.h"
+ 
+ void app_main(void)
+ {
+     // Initialize NVS
+     esp_err_t ret = nvs_flash_init();
+     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+     {
+         ESP_ERROR_CHECK(nvs_flash_erase());
+         ret = nvs_flash_init();
+     }
+     ESP_ERROR_CHECK(ret);
+ 
+     // Start Wifi
+     wifi_app_start();
+ }
+ 
